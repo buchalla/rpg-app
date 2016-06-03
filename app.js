a0b8f8bc1e82b15b60ab2872d39c0eb77bc7654f
Victor Schulze Buchalla <vs_buchalla@hotmail.com>
2016-06-03 18:04:02 +0000

    Mongose

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
+'use strict';
 const express = require('express');
 const app = express();
 const RouterLoader = require('./helpers/RouterLoader');
 const configs = require('./configs');
-
+const mongoose = require('mongoose');
 const rl = new RouterLoader(app, configs.folder.api);
 rl.requireRecursively();
 
-app.listen( process.env.PORT || 3000, function(){
+
+app.listen( process.env.PORT || 3000, () => {
     console.log('server listen on port', process.env.PORT || 3000);
 });
--- configs.js
+++ configs.js
@@ -5,7 +5,6 @@ module.exports = {
         controller: `${__dirname}/controller/`,
         dao: `${__dirname}/dao/`,
         helpers: `${__dirname}/helpers/`,
-        middleware: `${__dirname}/middleware/`,
         model: `${__dirname}/model/`
     }
 };
